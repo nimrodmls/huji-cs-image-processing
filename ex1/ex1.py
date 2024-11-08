0e13f01e4864176106ea15cb00fb5e61e629346d
Nimrod Mallis <nimrodmallis@gmail.com>
2024-11-08 14:30:02 +0200

    Added skeleton file from Moodle

---
+++ ex1/ex1.py
@@ -0,0 +1,9 @@
+
+def main(video_path, video_type):
+    """
+    Main entry point for ex1
+    :param video_path: path to video file
+    :param video_type: category of the video (either 1 or 2)
+    :return: a tuple of integers representing the frame number for which the scene cut was detected (i.e. the last frame index of the first scene and the first frame index of the second scene)
+    """
+    pass
