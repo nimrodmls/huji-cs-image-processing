f697c73371cab71b3390371e02ef7212a14ca48d
Nimrod Mallis <nimrodmallis@gmail.com>
2024-12-03 13:32:24 +0200

    Final fixes and additions ahead of submission

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -1,9 +1,13 @@
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import ShortTimeFFT, windows, find_peaks
 from scipy.fft import fft, fftfreq
 
+task2_watermarked_files = ['inputs\\0_watermarked.wav', 'inputs\\1_watermarked.wav', 'inputs\\2_watermarked.wav',
+                         'inputs\\3_watermarked.wav', 'inputs\\4_watermarked.wav', 'inputs\\5_watermarked.wav',
+                         'inputs\\6_watermarked.wav', 'inputs\\7_watermarked.wav', 'inputs\\8_watermarked.wav',]
 
 ### STFT Hyper Parameters ###
 
@@ -14,12 +18,31 @@ window = windows.hann(window_size)
 # STFT Parameters
 stft_hop = 32
 
-def visualize_spectrogram(in_signal: np.array, sample_rate: int, dest_filename: str):
+def visualize_spectrogram(in_signal: np.array, sample_rate: int, dest_filename: str, title: str=''):
     SFT = ShortTimeFFT(window, stft_hop, sample_rate, mfft=window_size*2, scale_to='magnitude')
     plt.imshow(np.log(SFT.spectrogram(in_signal) + 1), aspect='auto', origin='lower', cmap='magma', extent=SFT.extent(in_signal.shape[0]))
+    plt.title(title)
+    plt.colorbar(label='Magnitude [dB]')
     plt.xlabel('Time [s]')
     plt.ylabel('Frequency [Hz]')
     plt.savefig(dest_filename)
+    plt.close()
+
+def visualize_task2_spectrograms():
+    """
+    """
+    audio_files = [(wavfile.read(file), file) for file in task2_watermarked_files]    
+    sample_rate = audio_files[0][0][0] # Assuming the sampling rate is the same for each file
+
+    SFT = ShortTimeFFT(window, stft_hop, sample_rate, mfft=window_size*2, scale_to='magnitude')
+    fig, axs = plt.subplots(3, 3, figsize=(20, 20), sharex=True)
+    for i in range(3):
+        for j, (in_signal, title) in enumerate(audio_files[i*3:i*3+3]):
+            axs[i][j%3].imshow(np.log(SFT.spectrogram(in_signal[1]) + 1), aspect='auto', origin='lower', cmap='viridis', extent=SFT.extent(in_signal[1].shape[0]))
+            axs[i][j%3].set_title(title)
+            axs[i][j%3].set_ylabel('Frequency [Hz]')
+            axs[i][j%3].set_xlabel('Time [s]')
+    plt.savefig("task2_spectrograms.png")
     plt.close()
 
 def add_constant_frequency(in_audio, sample_rate, frequency, amplitude):
@@ -34,9 +57,6 @@ def add_constant_frequency(in_audio, sample_rate, frequency, amplitude):
     
     # Dealing with overflow
     out_data = np.clip(out_data, -32768, 32767).astype(np.int16)
-    
-    # Create spectrogram
-    visualize_spectrogram(out_data, sample_rate, "original_task1_spec.png")
 
     return out_data
 
@@ -56,33 +76,33 @@ def task1():
     amplitude = 1000  # Amplitude for the added frequency
 
     sample_rate, in_audio = wavfile.read('inputs\\task1.wav')
+    visualize_spectrogram(in_audio, sample_rate, "original_task1_spec.png", "Original Audio")
 
     # Creating a good watermark
     good_frequency = 18000  # The added frequency in Hz - Choosing a high frequency to avoid human hearing
     out_audio = add_constant_frequency(in_audio, sample_rate, good_frequency, amplitude)
     wavfile.write('task1_good_watermark.wav', sample_rate, out_audio)
+    visualize_spectrogram(out_audio, sample_rate, "good_task1_spec.png", "Good Watermark")
 
     # Creating a bad watermark
     bad_frequency = 1000  # The added frequency in Hz - Choosing a low frequency to be heard
     out_audio = add_constant_frequency(in_audio, sample_rate, bad_frequency, amplitude)
     wavfile.write('task1_bad_watermark.wav', sample_rate, out_audio)
+    visualize_spectrogram(out_audio, sample_rate, "bad_task1_spec.png", "Bad Watermark")
 
 def task2():
     """
     """
-    watermarked_files = ['inputs\\0_watermarked.wav', 'inputs\\1_watermarked.wav', 'inputs\\2_watermarked.wav',
-                         'inputs\\3_watermarked.wav', 'inputs\\4_watermarked.wav', 'inputs\\5_watermarked.wav',
-                         'inputs\\6_watermarked.wav', 'inputs\\7_watermarked.wav', 'inputs\\8_watermarked.wav',]
-    
     ### Hyperparameters
-
     # {Distance Between Peaks: Applicable Watermark Category}
     peaks_distancing = {220: 1, 155: 2, 120: 3}
     # The frequency threshold from which the watermark starts (mostly without noise, in the given examples)
     freq_threshold = 19500
 
+    visualize_task2_spectrograms()
+
     # Categorizing each of the watermarked files
-    for file in watermarked_files:
+    for file in task2_watermarked_files:
 
         # Reading the watermarked file and taking its spectrogram (squared absolute values of stft)
         sample_rate, in_audio = wavfile.read(file)
@@ -108,39 +128,14 @@ def task2():
             if (peaks_diff < 40).sum() == peaks_diff.shape[0]:
                print(f'{file} is category {cat} - Frequency: {identified_freq}')
                break
-        
-        # Visualizations - TODO: delete later
-        # plt.plot(a)
-        # plt.title(file)
-        # plt.xlabel('Time [s]')
-        # plt.ylabel('Amplitude')
-        # plt.savefig(file.replace('.wav', '_amps.png'))
-        # plt.close()
-        #visualize_spectrogram(in_audio, sample_rate, file.replace('.wav', '.png'))
 
 def task3():
     """
     """
-    # speedup_files = ['inputs\\task3_watermarked_method1.wav', 'inputs\\task3_watermarked_method2.wav']
-    # # Handling the file slowed down in the time domain
-    # file_path = 'inputs\\task3_watermarked_method1.wav'
-    # sample_rate, in_audio = wavfile.read(file_path)
-    # visualize_spectrogram(in_audio, sample_rate, file_path.replace('.wav', '.png'))
-    # wavfile.write(file_path.replace('.wav', '_speedup.wav'), sample_rate * 4, in_audio)
-
-    # # Handling the file slowed down in the frequency domain
-    # file_path = 'inputs\\task3_watermarked_method2.wav'
-    # sample_rate, in_audio = wavfile.read(file_path)
-    # SFT = ShortTimeFFT(window, fs=sample_rate, hop=stft_hop, mfft=window_size*2, scale_to='magnitude')
-    # stft_ret = SFT.stft(in_audio)
-    # stft_ret *= 4
-    # in_audio = SFT.istft(stft_ret)
-    # in_audio = np.clip(in_audio, -32768, 32767).astype(np.int16)
-    # visualize_spectrogram(in_audio, sample_rate, file_path.replace('.wav', '_fixed.png'))
-    # wavfile.write(file_path.replace('.wav', '_speedup.wav'), sample_rate, in_audio)    
-
     sample_rate1, in_audio1 = wavfile.read('inputs\\task3_watermarked_method1.wav')
+    visualize_spectrogram(in_audio1, sample_rate1, "task3_watermarked_method1_spec.png", "Method 1 Watermark")
     sample_rate2, in_audio2 = wavfile.read('inputs\\task3_watermarked_method2.wav')
+    visualize_spectrogram(in_audio2, sample_rate2, "task3_watermarked_method2_spec.png", "Method 2 Watermark")
 
     # File 1 has a lower sample rate, so we match the sample rate of file 2 (it's a factor of 4)
     # for simpler spectrogram analysis
@@ -164,9 +159,7 @@ def task3():
     else:
         print(f'File 2 - Time Domain, File 1 - Frequency Domain, factor {sample_rate2 / sample_rate1}')
 
-if __name__ == "__main__":
-    #main()
-    
-    # task1()
-    #task2()
+if __name__ == "__main__":    
+    task1()
+    task2()
     task3()
