85ac1260e540697256001432fb68524ddeb56b61
Nimrod Mallis <nimrodmallis@gmail.com>
2024-12-26 17:28:32 +0200

    Implemented homography calculation using SIFT

--- ex4/ex4.py
+++ ex4/ex4.py
@@ -10,29 +10,45 @@ transformation matrix for each transition.
 """
 
 import mediapy
+import cv2
 import numpy as np
 from scipy.signal import convolve
 
 BOAT_INPUT_VIDEO_PATH = "inputs\\boat.mp4"
 
-def calculate_homography_matrix(frame1, frame2):
+def calculate_transformation(frame1, frame2):
     """
-    Calculate the homography matrix for the given frames.
     """
-    # Compute derivatives
+    sift = cv2.SIFT_create()
+    f1_kp, f1_desc = sift.detectAndCompute(frame1, None)
+    f2_kp, f2_desc = sift.detectAndCompute(frame2, None)
 
+    feature_matcher = cv2.BFMatcher()
+    matches = feature_matcher.knnMatch(f1_desc, f2_desc, k=2)
 
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
 
-    # Compute the LK matrix
+    f1_points = np.float32([f1_kp[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+    f2_points = np.float32([f2_kp[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
 
-    # Iterating until convergence
-    pass
+    homography_matrix, _ = cv2.findHomography(f1_points, f2_points, cv2.RANSAC, 5.0)
+    return homography_matrix 
 
 def read_video(path: str):
     return mediapy.read_video(path)
 
+def debug_create_still_video(image):
+    """
+    Creating a still video from an single image, consisting of 100 frames
+    """
+    return np.array([image for _ in range(100)])
+
 def main():
-    video = read_video(BOAT_INPUT_VIDEO_PATH)
+    video = debug_create_still_video(mediapy.read_image("gus-fring.png"))
+    # video = read_video(BOAT_INPUT_VIDEO_PATH)
     
     # Iterate over consecutive pairs of frames
     for idx, frame in enumerate(video[:-1]):
@@ -40,7 +56,7 @@ def main():
         frame2 = video[idx+1]
 
         # Calculate the homography matrix
-        homography_matrix = calculate_homography_matrix(frame1, frame2)
+        homography_matrix = calculate_transformation(frame1, frame2)
 
         # Apply the homography matrix to the second frame
         # to align it with the first frame
